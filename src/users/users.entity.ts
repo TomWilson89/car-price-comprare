c602881586c02c257179cff3f25a5c5c10b5f6de
tomwilson89 <tfuenteswilson@gmail.com>
2022-01-04 14:52:41 -0300

    feat: exclude password from any response

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,13 +1,16 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Index()
   @Column()
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 }
