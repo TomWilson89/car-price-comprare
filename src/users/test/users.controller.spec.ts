39fc2839d0762097f988213af85b4b63a8fd44f7
tomwilson89 <tfuenteswilson@gmail.com>
2022-01-06 08:52:01 -0300

    test: ensure UsersController is defined

--- src/users/test/users.controller.spec.ts
+++ src/users/test/users.controller.spec.ts
@@ -1,23 +1,35 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { UsersController } from './users.controller';
+import { Test, TestingModule } from '@nestjs/testing';
+import { mock, MockProxy } from 'jest-mock-extended';
+import { AuthService } from '../auth.service';
+import { UsersController } from '../users.controller';
+import { UsersService } from '../users.service';
 
-// describe.skip('UsersController', () => {
-//   let controller: UsersController;
-
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [UsersController],
-//     }).compile();
-
-//     controller = module.get<UsersController>(UsersController);
-//   });
-
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
 describe('UsersController', () => {
-  test('should ', () => {
-    expect(true).toBe(true);
+  let sut: UsersController;
+  let mockUsersService: MockProxy<UsersService>;
+  let mockAuthService: MockProxy<AuthService>;
+
+  beforeEach(async () => {
+    mockUsersService = mock();
+    mockAuthService = mock();
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
+    }).compile();
+
+    sut = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
   });
 });
