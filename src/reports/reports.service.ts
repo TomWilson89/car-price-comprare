943a0b73dd64fa107732cd4523f3d21e4111dc65
tomwilson89 <tfuenteswilson@gmail.com>
2022-01-07 08:25:46 -0300

    feat: create user entity, controller and service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report';
+import { Report } from './reports.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report)
+    private readonly reportsRepository: Repository<Report>,
+  ) {}
+
+  async create(reportDto: CreateReportDto) {
+    const report = this.reportsRepository.create(reportDto);
+    return await this.reportsRepository.save(report);
+  }
+}
