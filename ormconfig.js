47017d8129a027e93b3ffde7738c699a95db3774
tomwilson89 <tfuenteswilson@gmail.com>
2022-01-09 18:27:27 -0300

    feat: create production config

--- ormconfig.js
+++ ormconfig.js
@@ -25,6 +25,17 @@ switch (process.env.NODE_ENV) {
     });
     break;
   case 'production':
+    Object.assign(dbConfig, {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      migrations: [`src/migrations/**/*.js`],
+      entities: [__dirname + '/**/*.entity.js'],
+      migrationsRun: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+
     break;
 
   default:
